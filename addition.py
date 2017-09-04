c343463229897a2f457e96e2e02e39c4ee688788
shreyash7975 <31614728+shreyash7975@users.noreply.github.com>
2017-09-04 14:16:34 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,12 @@
+Python 3.6.2 (default, Jul 20 2017, 08:43:29) 
+[GCC 6.3.0 20170406] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=5
+>>> print (a+b)
+10
+>>> a=3
+>>> b=3
+>>> print(a*b)
+9
+>>> 
