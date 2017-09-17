a1030d46cf2e219909a026a33f576cb7238c4210
shreyash7975 <31614728+shreyash7975@users.noreply.github.com>
2017-09-17 13:52:48 +0530

    Delete email2.py

--- email2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import smtplib
-s = smtplib.SMTP('smtp.uyash7975@gmail.com',587)
-s.startls()
-s.login("uyash7975@gmail.com","shreyash7975","")
-message= "hello"
-s.sendmail("uyash7975@gmail.com","uyash@79752gmail.com",message)
-s.quit()s
