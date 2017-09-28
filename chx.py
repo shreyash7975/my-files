e6d359a2ae468b8c3a9c9132c4fc5a9134604935
shreyash7975 <31614728+shreyash7975@users.noreply.github.com>
2017-09-28 09:06:02 +0530

    Update chx.py

--- chx.py
+++ chx.py
@@ -6,14 +6,17 @@ consumer_key ="FfqzyAvxeBbxqFZRndF3XH09G"
 consumer_secret ="k9HSs6R1YES9oqDckXuPfS52AtKIBxFfQz7FvrnjDDsFExRFW8"
 access_token ="912244949390733312-J7mCs4rFe0ZPgdCyfM4HagcaPLjOsty"
 access_token_secret ="SRNTmSyFnFRUIo6GiH4x2z5utkuKd1WqREvbt85XXkU4Z"
+#from line 5 to 8 all the tokens are to be created in the "createapplication in the twitter official page" 
  
 # authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
   
 api = tweepy.API(auth)
-tweet ="hallaluya" 
+tweet ="hallaluya"
+#tweet is the text message that has to be comented on command
 image_path ="/home/pi/Desktop/charya1.jpg" 
+#is the image path
  
 
 status = api.update_with_media(image_path, tweet) 
