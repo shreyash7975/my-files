175f79c27a2f702f186d3d3fe170c924975db77b
shreyash7975 <31614728+shreyash7975@users.noreply.github.com>
2017-09-28 08:48:37 +0530

    Update tables.py

--- tables.py
+++ tables.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python3
 num  = int(input("enter the number : "))
+#number defines multiple number
 for  i in range (1,4):
+#i is the rangewhich is the table
 	for j in range (1,11):
+#j is the range from 1 to 10 
 		print (i,"x",j,"=",i*j)
+#*is multiplication
